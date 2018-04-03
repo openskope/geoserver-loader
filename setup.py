d0550f7de974f241ecf203a7ba750258a3273eb5
Jeff Terstriep <jefft@illinois.edu>
2018-04-03 16:31:04 +0000

    Add setup.py to improve installation.

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+def readme():
+    try:
+        with open('README.md') as f:
+            return f.read()
+    except IOError as e:
+        return ''
+
+
+setup(
+    name="skope_utils",
+    version="1.0.0",
+    author="Jeff Terstriep",
+    author_email="jefft@illinois.edu",
+    description="Collection of scripts for interacting with SKOPE",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        'gsconfig',
+        'rasterio>=1.0a12',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    scripts=[
+        'scripts/load_paleocar.py',
+        'scripts/convert.sh'
+    ]
+
+)
+
+        
