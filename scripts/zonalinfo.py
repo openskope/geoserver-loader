d2c67e8d7440e00d7e1dfb6815d071701c32e60e
Jeff Terstriep <jefft@illinois.edu>
2018-04-06 00:01:43 +0000

    Fix mask of nodata values from raster source.

--- scripts/zonalinfo.py
+++ scripts/zonalinfo.py
@@ -5,7 +5,7 @@ generate zonal statistics for a single region. Inputs include a raster
 file such as a GeoTiff and a geojson-like file or string. Only Point
 and Polygon GeoJSON types are currently supported.
 
-The stats generated can include mean, min, max, and sd. A pseudo-method
+The stats generated can include mean, min, max, and std. A pseudo-method
 called 'standard' will generate all 4 statistics.
 
 If the raster contains multiple bands the tools will loop through each band
@@ -47,12 +47,12 @@ def read_geometry(fname):
 
 
 def write_json(results):
-    for arr in 'mean min max'.split():
+    for arr in 'mean min max std'.split():
         if arr in results.keys():
-            results[arr] = results[arr].tolist()
+            results[arr] = results[arr]
 
     if 'window' in results.keys():
-        results['window'] = results['window'].todict()
+        results['window'] = results['window']
 
     sys.stdout.write(json.dumps(results))
 
@@ -61,8 +61,8 @@ def write_values(results, method):
 
     for i in range(results['count']):
         if method == 'standard':
-            print('%3.6f, %3.6f, %3.6f' % (results['mean'][i],
-                results['min'][i], results['max'][i]))
+            print('%3.6f, %3.6f, %3.6f %3.6f' % (results['mean'][i],
+                results['min'][i], results['max'][i]), results['std'])
         else:
             print('%3.6f' % (results[method][i]))
 
@@ -74,8 +74,9 @@ def write_gdallocationinfo(results, method):
     for i in range(results['count']):
         print('  Band %d:' % (i+1))
         if method == 'standard':
-            print('    Values: %3.6f, %3.6f, %3.6f' % (results['mean'][i],
-                    results['min'][i], results['max'][i]))
+            print('    Values: %3.6f, %3.6f, %3.6f, %3.6f' % (
+                    results['mean'][i], results['min'][i], 
+                    results['max'][i], results['std'][i]))
         else:
             print('    Value: %3.6f' % results[method][i])
 
@@ -108,20 +109,32 @@ def main():
     # currently handling a single geometry at a time
     geoms = [read_geometry(args.geometry)]
 
+    if args.method == 'standard':
+        zonal = dict(mean=[], min=[], max=[], std=[])
+    else:
+        zonal = { args.method: [] }
+
     with rasterio.open(args.raster) as src:
-        mask, transform, window = raster_geometry_mask(src, geoms, crop=True)
-        zonal = {'window': window, 'count': src.count }
+        masked, transform, window = raster_geometry_mask(src, geoms, crop=True)
+        zonal['window'] = window
+        zonal['count'] = src.count
         zonal['band'] = range(src.count)
 
-        values = src.read(None, window=window)
+        for i in range(src.count):
+            data = src.read(i+1, window=window)
+            values = np.ma.array(data=data, 
+                    mask=np.logical_or(np.equal(data, src.nodata), masked))
 
-        if args.method == 'mean' or args.method == 'standard':
-            zonal['mean'] = np.mean(~mask[None,:,:] * values, axis=(1,2))
-        if args.method == 'min' or args.method == 'standard':
-            zonal['min'] = np.min(~mask[None,:,:] * values, axis=(1,2))
-        if args.method == 'max' or args.method == 'standard':
-            zonal['max'] = np.max(~mask[None,:,:] * values, axis=(1,2))
+            if args.method == 'mean' or args.method == 'standard':
+                zonal['mean'].append(np.mean(values))
+            if args.method == 'min' or args.method == 'standard':
+                zonal['min'].append(np.min(values))
+            if args.method == 'max' or args.method == 'standard':
+                zonal['max'].append(np.max(values))
+            if args.method == 'std' or args.method == 'standard':
+                zonal['std'].append(np.std(values))
 
+    
     if args.json:
         write_json(zonal)
 
